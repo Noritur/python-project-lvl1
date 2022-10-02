b3cd53413d0165567283980a8a1afcc4848bdaf9
Nikola <hevemahtalaran@gmail.com>
2022-10-02 15:43:59 +0300

    update terms_name.py

--- brain_games/terms_name.py
+++ brain_games/terms_name.py
@@ -5,7 +5,8 @@ prime_terms = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 when_wrong_answer = ' is wrong answer ;(. Correct answer was '
 
 
-even_terms = 'Answer "yes" if the number is even, otherwise answer "no".'
+def even_terms():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
 gcd_terms = 'Find the greatest common divisor of given numbers.'
