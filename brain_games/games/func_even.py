ce9dc13b73214b86eac3de41115891ba6e4fd1a5
Nikola <hevemahtalaran@gmail.com>
2022-10-02 20:22:34 +0300

    update func even code climate

--- brain_games/games/func_even.py
+++ brain_games/games/func_even.py
@@ -8,20 +8,23 @@ def even_game():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     even_terms()
-    count = 0
-    while count <= 2:
-        random_number = randint(1, 200)
-        print(f'Question: {random_number}')
-        if random_number % 2 == 0:
-            even_or_not = 'yes'
-        else:
-            even_or_not = 'no'
-        answer = prompt.string('Your answer: ')
-        if answer == even_or_not:
-            print('Correct!')
-            count += 1
-        else:
-            print(f"'{answer}'{when_wrong_answer}'{even_or_not}'.")
-            print(f"Let's try again, {name}!")
-            return
-    return print(f'Congratulations, {name}!')
+
+    def even():
+        count = 0
+        while count <= 2:
+            random_number = randint(1, 200)
+            print(f'Question: {random_number}')
+            if random_number % 2 == 0:
+                even_or_not = 'yes'
+            else:
+                even_or_not = 'no'
+            answer = prompt.string('Your answer: ')
+            if answer == even_or_not:
+                print('Correct!')
+                count += 1
+            else:
+                print(f"'{answer}'{when_wrong_answer}'{even_or_not}'.")
+                print(f"Let's try again, {name}!")
+                return
+        return print(f'Congratulations, {name}!')
+    even()
