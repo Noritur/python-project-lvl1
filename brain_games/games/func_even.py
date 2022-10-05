46962a58473ab90f9a085412eda3c85921a95829
Nikola <hevemahtalaran@gmail.com>
2022-10-05 19:29:20 +0300

    fix logic2

--- brain_games/games/func_even.py
+++ brain_games/games/func_even.py
@@ -3,13 +3,13 @@ from random import randint
 GAME_TERMS = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even_game(random_number):
-    if random_number % 2 == 0:
+def is_even_game(question):
+    if question % 2 == 0:
         return 'yes'
     return 'no'
 
 
 def logic_game():
-    random_number = randint(1, 200)
-    correct_answer = is_even_game(random_number)
-    return random_number, correct_answer
+    question = randint(1, 200)
+    correct_answer = is_even_game(question)
+    return question, correct_answer
