1bea0933b4269e06f622a3e28b1c94fe5ab74a65
Nikola <hevemahtalaran@gmail.com>
2022-10-07 18:00:27 +0300

    fix func gcd

--- brain_games/games/func_gcd.py
+++ brain_games/games/func_gcd.py
@@ -7,6 +7,6 @@ GAME_TERMS = 'Find the greatest common divisor of given numbers.'
 def logic_game():
     rand_number1 = randint(1, 100)
     rand_number2 = randint(1, 100)
-    correct_answer = math.gcd(rand_number1, rand_number2)
+    correct_answer = str(math.gcd(rand_number1, rand_number2))
     question = f'{rand_number1} {rand_number2}'
     return question, correct_answer
